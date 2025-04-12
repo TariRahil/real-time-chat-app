fd9015ef0b9cd985d4ec26a92627eaa680a17e3c
RahilTari <RahilTari@gmail.com>
2025-04-12 15:35:32 +0330

    Create JWT util for security.

--- auth-service/src/main/java/authService/security/JwtUtil.java
+++ auth-service/src/main/java/authService/security/JwtUtil.java
@@ -1,2 +1,43 @@
-package authService.security;public class JwtUtil {
+package authService.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean validateToken(String token, String username) {
+        final String extractedUsername = extractClaims(token).getSubject();
+        return (extractedUsername.equals(username) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
 }
