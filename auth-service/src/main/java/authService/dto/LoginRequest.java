7cea8c80a413f1b438dc657be25db15d188687f2
RahilTari <RahilTari@gmail.com>
2025-04-12 15:33:06 +0330

    Create login request dto.

--- auth-service/src/main/java/authService/dto/LoginRequest.java
+++ auth-service/src/main/java/authService/dto/LoginRequest.java
@@ -1,2 +1,9 @@
-package authService.dto;public class LoginRequest {
+package authService.dto;
+
+import lombok.Data;
+
+public @Data
+class LoginRequest {
+    private String username;
+    private String password;
 }
