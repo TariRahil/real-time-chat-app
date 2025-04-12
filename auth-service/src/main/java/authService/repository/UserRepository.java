7eb79717b533332fdf2e6a2ba5db6012753c79f1
RahilTari <RahilTari@gmail.com>
2025-04-12 15:34:09 +0330

    Create user repository.

--- auth-service/src/main/java/authService/repository/UserRepository.java
+++ auth-service/src/main/java/authService/repository/UserRepository.java
@@ -1,2 +1,11 @@
-package authService.repository;public class UserRepository {
+package authService.repository;
+
+import authService.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
