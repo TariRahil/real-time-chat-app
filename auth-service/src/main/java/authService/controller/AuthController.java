e2be0f36ec18b786d73b9ac61275ac0e5d9425f5
RahilTari <RahilTari@gmail.com>
2025-04-12 15:32:01 +0330

    Create auth controller.

--- auth-service/src/main/java/authService/controller/AuthController.java
+++ auth-service/src/main/java/authService/controller/AuthController.java
@@ -1,2 +1,59 @@
-package authService.controller;public class AuthController {
+package authService.controller;
+
+import authService.dto.LoginRequest;
+import authService.dto.LoginResponse;
+import authService.model.User;
+import authService.repository.UserRepository;
+import authService.security.JwtUtil;
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(UserRepository userRepository, JwtUtil jwtUtil, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.jwtUtil = jwtUtil;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody User user) {
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            return ResponseEntity.badRequest().body("Username already exists.");
+        }
+
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            return ResponseEntity.badRequest().body("Email already exists.");
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+
+        return ResponseEntity.ok("User Registered successfully.");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        User user = userRepository.findByUsername(loginRequest.getUsername())
+                .orElseThrow(() -> new RuntimeException("User not found."));
+
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            return ResponseEntity.status(401).body("Invalid credentials.");
+        }
+
+        String token = jwtUtil.generateToken(user.getUsername());
+
+        return ResponseEntity.ok(new LoginResponse(token));
+    }
 }
